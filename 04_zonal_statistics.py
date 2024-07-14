d899e7cfcd9b8ca5c709b0abd18105d2e9285d2d
Queeno11 <abbatenicolas@gmail.com>
2024-07-14 18:03:01 -0300

    separate processing and compiling

--- 04_zonal_statistics.py
+++ 04_zonal_statistics.py
@@ -62,66 +62,76 @@ if __name__ == "__main__":
 
         print(f"Processing {shockname}...")
 
-        # shock, needs_interp, needs_coarsen = utils.identify_needed_transformations(
-        #     shock, adm_id_full
-        # )
+        shock, needs_interp, needs_coarsen = utils.identify_needed_transformations(
+            shock, adm_id_full
+        )
 
         ## Loop over chunks
         #   Data is dividied in chunks (sections of the world) to avoid memory issues
         #   and to allow parallel processing. This loop will iterate over every chunk
-        # for chunk_number in tqdm(range(TOTAL_CHUNKS)):
-        #     chunk_start_time = time.time()
+        for chunk_number in tqdm(range(TOTAL_CHUNKS)):
+            chunk_start_time = time.time()
 
-        #     datafilter, chunk_bounds = utils.get_filter_from_chunk_number(
-        #         chunk_number, total_chunks=TOTAL_CHUNKS, canvas=WB_data.total_bounds
-        #     )
-        #     chunk_shock = shock.sel(datafilter).load()
-        #     chunk_adm_id = adm_id_full.sel(datafilter)
-        #     if chunk_adm_id.notnull().sum() == 0:
-        #         print("No data in this chunk, skipping...")
-        #         continue
+            datafilter, chunk_bounds = utils.get_filter_from_chunk_number(
+                chunk_number, total_chunks=TOTAL_CHUNKS, canvas=WB_data.total_bounds
+            )
+            chunk_shock = shock.sel(datafilter).load()
+            chunk_adm_id = adm_id_full.sel(datafilter)
+            if chunk_adm_id.notnull().sum() == 0:
+                print("No data in this chunk, skipping...")
+                continue
 
-        #     ## Loop over years
-        #     # Note: data in this NC file will query faster if chunked in the same way as the data is stored
-        #     #   so loading the chunks based on lat-lon will be fast. Once in memory, we can slice by year and
-        #     #   send to cupy faster. That's why we loop over chunks first and then years.
-        #     for year in tqdm(shock.year.values, leave=False):
-        #         gpw_year_prev = 0
-        #         chunk_year_shock = chunk_shock.sel(year=year)
+            ## Loop over years
+            # Note: data in this NC file will query faster if chunked in the same way as the data is stored
+            #   so loading the chunks based on lat-lon will be fast. Once in memory, we can slice by year and
+            #   send to cupy faster. That's why we loop over chunks first and then years.
+            for year in tqdm(shock.year.values, leave=False):
+                gpw_year_prev = 0
+                chunk_year_shock = chunk_shock.sel(year=year)
 
-        #         gpw_year = utils.find_gpw_closes_year(year)
-        #         if gpw_year != gpw_year_prev:
-        #             # Load Population data as cupy array
-        #             chunk_year_gpw = xr.open_dataarray(
-        #                 rf"{GPW_PATH}/gpw_v4_population_count_rev11_{gpw_year}_30_sec.tif"
-        #             )
-        #             chunk_year_gpw = chunk_year_gpw.sel(band=1).sel(datafilter)
-        #             chunk_year_gpw = cp.asarray(chunk_year_gpw.values)
+                gpw_year = utils.find_gpw_closes_year(year)
+                if gpw_year != gpw_year_prev:
+                    # Load Population data as cupy array
+                    chunk_year_gpw = xr.open_dataarray(
+                        rf"{GPW_PATH}/gpw_v4_population_count_rev11_{gpw_year}_30_sec.tif"
+                    )
+                    chunk_year_gpw = chunk_year_gpw.sel(band=1).sel(datafilter)
+                    chunk_year_gpw = cp.asarray(chunk_year_gpw.values)
 
-        #         ## Loop over variables
-        #         for var in tqdm(shock.data_vars, leave=False):
+                ## Loop over variables
+                for var in tqdm(shock.data_vars, leave=False):
 
-        #             datavar = chunk_year_shock[var]
-        #             if needs_interp:
-        #                 datavar = utils.intepolate_era5_data(
-        #                     datavar,
-        #                     chunk_adm_id,
-        #                     verbose=False,
-        #                 )
-        #             else:
-        #                 datavar = datavar.values
+                    datavar = chunk_year_shock[var]
+                    if needs_interp:
+                        datavar = utils.intepolate_era5_data(
+                            datavar,
+                            chunk_adm_id,
+                            verbose=False,
+                        )
+                    else:
+                        datavar = datavar.values
 
-        #             ### Groupby
-        #             df = utils.compute_zonal_statistics(
-        #                 datavar, chunk_adm_id, chunk_year_gpw
-        #             )
-        #             df.to_parquet(
-        #                 rf"{PARQUET_PATH}/{shockname}_{var}_{year}_{chunk_number}_zonal_stats.parquet"
-        #             )
+                    ### Groupby
+                    df = utils.compute_zonal_statistics(
+                        datavar, chunk_adm_id, chunk_year_gpw
+                    )
+                    df.to_parquet(
+                        rf"{PARQUET_PATH}/{shockname}_{var}_{year}_{chunk_number}_zonal_stats.parquet"
+                    )
+
+    adm_id_full = None
+    chunk_shock = None
+    chunk_adm_id = None
+    chunk_year_shock = None
+    chunk_year_gpw = None
+    datavar = None
+
+    for shockname, shock in shocks.items():
+
+        print(f"Exporting {shockname}...")
 
         ## Save shock data
         # Compile all the dataframes and generate country dtas
-        print("Compiling data...")
         out_df, variables = utils.process_all_dataframes(
             WB_data, PARQUET_PATH, shockname
         )
