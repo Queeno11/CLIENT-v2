e93643c4501d994cd7354b4183697063e4f39565
Nicolás Abbate <57244513+Queeno11@users.noreply.github.com>
2025-06-23 11:35:51 -0300

    add argparse to exporta_bases_web

--- 06_exporta_bases_web.py
+++ 06_exporta_bases_web.py
@@ -33,20 +33,29 @@ def genera_mapa_climate_dashboard():
     '''
     # Load ID dataset
     gdf = gpd.read_feather(rf"{DATA_PROC}\\WB_country_IDs.feather")
-    gdf.columns = gdf.columns.str.lower()
-    gdf = gdf.rename(columns={"id":"ID"}).drop(columns="objectid")
 
     # Add names from the original WB adm2 dataset
     gdf_raw = procesa_bases.load_WB_country_data()
-    gdf_raw.columns = gdf_raw.columns.str.lower()
-    gdf_raw = gdf_raw[["adm0_code", "adm1_code", "adm2_code", "adm0_name", "adm1_name", "adm2_name", "geometry"]]
-    assert gdf_raw.duplicated(subset=["adm0_code", "adm1_code", "adm2_code"]).sum() == 0, "There are duplicated entries in the raw dataset!!"
+    gdf_raw = gdf_raw[["ISO_A3", "ADM1CD_c", "ADM2CD_c", "geometry"]]
+    assert gdf_raw.duplicated(subset=["ISO_A3", "ADM1CD_c", "ADM2CD_c"]).sum() == 0, "There are duplicated entries in the raw dataset!!"
 
     # Merge both datasets to assert that the codes are correct and consistent
-    gdf = gdf.merge(gdf_raw.drop(columns="geometry"), how="outer", on=["adm0_code", "adm1_code", "adm2_code"], indicator=True, validate="1:1")
+    gdf = gdf.merge(gdf_raw.drop(columns="geometry"), how="outer", on=["ISO_A3", "ADM1CD_c", "ADM2CD_c"], indicator=True, validate="1:1")
     assert (gdf._merge == "both").all(), "There are problems with the merge!!"
     gdf = gdf.drop(columns="_merge")
 
+    # Rename for export
+    gdf = gdf.rename(
+        columns={
+            "ISO_A3": "adm0_code", 
+            "ADM1CD_c": "adm1_code", 
+            "ADM2CD_c": "adm2_code",
+            "NAM_0": "adm0_name",
+            "NAM_1": "adm1_name",
+            "NAM_2": "adm2_name",
+        }
+    )
+    print(gdf.columns)
     # Remove conflicting boundaries # FIXME: this should be implemented in the load_WB_country_data function...
     gdf = procesa_bases.fix_disputed_boundaries(gdf)
     
@@ -72,7 +81,6 @@ def genera_shocks_climate_dashboard(gdf):
         gdf (geopandas.GeoDataFrame): Base geográfica con información de los países y códigos administrativos.
 
     '''
-    
     # Set admin level to categorical dtype (when the dataset is expanded, it will be more memory efficient)
     gdf["ID"]        = gdf["ID"].astype("category")
     gdf["adm0_code"] = gdf["adm0_code"].astype("category")
@@ -392,66 +400,79 @@ def genera_zip():
             zipf.write(rf"{path}\{file}", arcname=file)
 
 if __name__ == "__main__":
+    import argparse
+        
+    parser = argparse.ArgumentParser(description="Generate dashboard data")
+    parser.add_argument("--db", choices=["climate", "ops", "hc", "all"], default="all", required=True, help="Choose the database to run: climate, ops, or hc")
+
+    args = parser.parse_args()
+
+    if (args.db == "climate") | (args.db == "all"):
+        print("Generando bases del Climate Dashboard")
+        gdf = genera_mapa_climate_dashboard()
+        genera_shocks_climate_dashboard(gdf)
+        gc.collect()
     
-    # print("Generando bases del Climate Dashboard")
-    # gdf = genera_mapa_climate_dashboard()
-    # genera_shocks_climate_dashboard(gdf)
-    # gc.collect()
+    if (args.db == "ops") | (args.db == "all"):
+        print("Generando bases del OPS Dashboard")
+        genera_shocks_subnacionales_ops_dashboard()
+        gc.collect() 
     
-    # print("Generando bases del OPS Dashboard")
-    # genera_shocks_subnacionales_ops_dashboard()
-    # gc.collect() 
-    
-    print("Generando bases del HC Dashboard")
-    # gdf_full = genera_mapa_hc_dashboard()
-    # genera_shocks_nacionales_hc_dashboard(gdf_full)
-    genera_shocks_subnacionales_hc_dashboard()
-    gc.collect()
+    if (args.db == "hc") | (args.db == "all"):
+        print("Generando bases del HC Dashboard")
+        gdf_full = genera_mapa_hc_dashboard()
+        genera_shocks_nacionales_hc_dashboard(gdf_full)
+        genera_shocks_subnacionales_hc_dashboard()
+        gc.collect()
 
     # genera_zip()
-    print("Listo! Datos exportados para las páginas web.")
+    print("Listo! Datos exportados para las páginas web.")    
     
+    print("#########################################################")
+    print("####      Testing the generated datasets:            ####")
+    print("#########################################################")
     
-    
-    
-    # print("#########################################################")
-    # print("####      Testing the generated datasets:            ####")
-    # print("#########################################################")
-    # print("Testing CLIMATE DATASET:")
-    # # Load the data
-    # gdf = pd.read_csv(rf"{DATA_OUT}\\for webpage\\WB_map.csv")
+    if (args.db == "climate") | (args.db == "all"):
 
-    # for shock in ["floods", "drought", "hurricanes", "intenserain", "heatwaves", "coldwaves"]:
-        
-    #     print("Verifying", shock)
-    #     df = pd.read_csv(rf"{DATA_OUT}\\for webpage\\WB_{shock}.csv")
-        
-    #     test_tools.assert_correct_colnames(df)
-    #     test_tools.assert_correct_shape(df, gdf)
-    #     test_tools.validate_climate_dataset(df, gdf)
-    #     if shock=="floods":
-    #         total_chunks=8**2
-    #     elif shock=="hurricanes":
-    #         total_chunks=6**2
-    #     else: 
-    #         total_chunks=4**2
+        print("Testing CLIMATE DATASET:")
+        # Load the data
+        gdf = pd.read_parquet(rf"{DATA_OUT}\\for webpage\\WB_map.parquet")
+
+        for shock in ["floods", "drought", "hurricanes", "intenserain", "heatwaves", "coldwaves"]:
             
-    #     test_tools.compare_xarray_with_zonal_statistics(adm="WB", chunk_number=None, shockname=shock, var=None, total_chunks=total_chunks, year=None, out_name=f"test_climate_{shock}")
-        
-    # print("Testing HC DATASET:")
+            print("Verifying", shock)
+            df = pd.read_csv(rf"{DATA_OUT}\\for webpage\\WB_{shock}.csv")
+            
+            test_tools.assert_correct_colnames(df)
+            test_tools.assert_correct_shape(df, gdf)
+            test_tools.validate_climate_dataset(df, gdf)
+            if shock=="floods":
+                total_chunks=8**2
+            elif shock=="hurricanes":
+                total_chunks=6**2
+            else: 
+                total_chunks=4**2
+                
+            test_tools.compare_xarray_with_zonal_statistics(adm="WB", chunk_number=None, shockname=shock, var=None, total_chunks=total_chunks, year=None, out_name=f"test_climate_{shock}")
     
-    # test_tools.validate_hc_merge()
-    # for shock in ["floods", "drought", "hurricanes", "intenserain", "heatwaves", "coldwaves"]:
-        
-    #     if shock=="floods":
-    #         total_chunks=8**2
-    #     elif shock=="hurricanes":
-    #         total_chunks=6**2
-    #     else: 
-    #         total_chunks=4**2
-    #     print(total_chunks)
-    #     test_tools.compare_xarray_with_zonal_statistics(adm="IPUMS", chunk_number=None, shockname=shock, var=None, total_chunks=total_chunks, year=None, out_name=f"test_hc_{shock}")
+    if (args.db == "ops") | (args.db == "all"):
+        print("TO DO: Test the OPS_geo_data.csv")
+    
+    if (args.db == "hc") | (args.db == "all"):
 
-    # print("TO DO: Test the OPS_geo_data.csv")
-    # print("All tests passed! Check de output images")
+        print("Testing HC DATASET:")
+        
+        test_tools.validate_hc_merge()
+        for shock in ["floods", "drought", "hurricanes", "intenserain", "heatwaves", "coldwaves"]:
+            
+            if shock=="floods":
+                total_chunks=8**2
+            elif shock=="hurricanes":
+                total_chunks=6**2
+            else: 
+                total_chunks=4**2
+            print(total_chunks)
+            test_tools.compare_xarray_with_zonal_statistics(adm="IPUMS", chunk_number=None, shockname=shock, var=None, total_chunks=total_chunks, year=None, out_name=f"test_hc_{shock}")
+
+    print("All tests passed! Check de output images")
     
